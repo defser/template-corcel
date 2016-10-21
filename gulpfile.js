e32a1199ed942b586cca38ea48f0088d5d24589e
Ricardo Dias <cadu.hd@gmail.com>
2016-10-21 10:45:22 -0200

    added browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,11 @@
-const elixir = require('laravel-elixir');
-
-//require('laravel-elixir-vue-2');
-require('laravel-elixir-imagemin');
-
-/*
-  |--------------------------------------------------------------------------
-  | Elixir Asset Management
-  |--------------------------------------------------------------------------
-  |
-  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
-  | for your Laravel application. By default, we are compiling the Sass
-  | file for our application, as well as publishing vendor resources.
-  |
-*/
+const elixir = require('laravel-elixir')
+require('laravel-elixir-imagemin')
 
 elixir(mix => {
   mix.sass('*.sass')
   .scripts('*.js')
-  .imagemin();
-  //.webpack('app.js');
-});
+  .imagemin()
+  .browserSync({
+    proxy: 'localhost:8000'
+  })
+})
