0158df7a152e2550287bd58ee8bb53f5ee8f654e
Ricardo Dias <cadu.hd@gmail.com>
2016-10-21 10:46:12 -0200

    added google maps integration

--- resources/assets/js/map.js
+++ resources/assets/js/map.js
@@ -1,7 +1,6 @@
 var map;
-function initMap() {
-
-
+function initMap() 
+{
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
     center: {lat: -30.0346471, lng: -51.217658400000005}
@@ -22,35 +21,4 @@ function initMap() {
   marker.addListener('click', function() {
     infowindow.open(map, marker);
   });
-
-
-//   var mapLocation = {lat: -30.0346471, lng: -51.217658400000005}
-
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 10,
-//     center: mapLocation,
-//     scrollwheel: false
-//   })
-
-//   var image = new google.maps.MarkerImage('/images/map-icon.png',
-//     new google.maps.Size(59, 65),
-//     new google.maps.Point(0, 0),
-//     new google.maps.Point(24, 42)
-//   );
-
-
-//   var marker = new google.maps.Marker({
-//     position: mapLocation,
-//     //icon: image,
-//     title: 'Hello World!'
-//   })
-
-//   map.addMarker({
-//         position: mapLocation,
-//         icon: image,
-//         title: 'Titulo',
-//         infoWindow: {
-//             content: '<p><strong>Titulo</strong><br/>rua xyz<br/><br/>	(55) 3333333 - teste@teste.com.br</p>'
-//         }
-//     });
 }
