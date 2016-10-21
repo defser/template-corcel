09a640d96aa5273e705e55e72c3d1ee530c1ec06
Ricardo Dias <cadu.hd@gmail.com>
2016-10-21 10:46:23 -0200

    home enhancements

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -42,57 +42,57 @@
         </div>
 
         <div class="container">
-            <div class="row p40">
+            <div class="about">
+                <div class="row p40">
 
-                <div class="col-md-6 col-xs-12">
-                    <h2>Titulo</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, optio, voluptates. Facilis nesciunt illo ullam, asperiores a quod nam deserunt sunt delectus, praesentium adipisci vitae, sed dolores perspiciatis amet id.</p>
+                    <div class="col-md-6 col-xs-12">
+                        <h2>Titulo</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, optio, voluptates. Facilis nesciunt illo ullam, asperiores a quod nam deserunt sunt delectus, praesentium adipisci vitae, sed dolores perspiciatis amet id.</p>
+                    </div>
+
+                    <div class="col-md-6 col-xs-12">
+                        <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=550%C3%97150&w=500&h=250" alt="">
+                    </div>
+
+                </div>
+                
+                <div class="row  p40">
+
+                    <div class="col-md-6 col-xs-12">
+                        <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=550%C3%97150&w=500&h=250" alt="">
+                    </div>
+
+                    <div class="col-md-6 col-xs-12">
+                        <h2>Titulo</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, optio, voluptates. Facilis nesciunt illo ullam, asperiores a quod nam deserunt sunt delectus, praesentium adipisci vitae, sed dolores perspiciatis amet id.</p>
+                    </div>
+
                 </div>
 
-                <div class="col-md-6 col-xs-12">
-                    <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=550%C3%97150&w=500&h=250" alt="">
+                <div class="row p40">
+                    <div class="col-md-4 col-xs-12">
+                        <h2>Texto 1</h2>
+                        <p>HTML is a markup language that is used for creating web pages. The HTML tutorial section will help you understand the basics of HTML, so that you can create your own web pages or website.</p>
+                        <p><a href="#/html-tutorial/" target="_blank" class="btn btn-success">Learn More &raquo;</a></p>
+                    </div>
+                    <div class="col-md-4 col-xs-12">
+                        <h2>CSS</h2>
+                        <p>CSS is used for describing the presentation of web pages. The CSS tutorial section will help you learn the essentials of CSS, so that you can fine control the style and layout of your HTML document.</p>
+                        <p><a href="#/css-tutorial/" target="_blank" class="btn btn-success">Learn More &raquo;</a></p>
+                    </div>
+                    <div class="col-md-4 col-xs-12">
+                        <h2>Bootstrap</h2>
+                        <p>Bootstrap is a powerful front-end framework for faster and easier web development. The Bootstrap tutorial section will help you learn the techniques of Bootstrap so that you can create web your own website.</p>
+                        <p><a href="#/twitter-bootstrap-tutorial/" target="_blank" class="btn btn-success">Learn More &raquo;</a></p>
+                    </div>
                 </div>
-
-            </div>
-            
-            <hr>
-            
-              <div class="row  p40">
-
-                <div class="col-md-6 col-xs-12">
-                    <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=550%C3%97150&w=500&h=250" alt="">
-                </div>
-
-                  <div class="col-md-6 col-xs-12">
-                    <h2>Titulo</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, optio, voluptates. Facilis nesciunt illo ullam, asperiores a quod nam deserunt sunt delectus, praesentium adipisci vitae, sed dolores perspiciatis amet id.</p>
-                </div>
-
-            </div>
-
-            <div class="row p40">
-                <div class="col-md-4 col-xs-12">
-                    <h2>HTML</h2>
-                    <p>HTML is a markup language that is used for creating web pages. The HTML tutorial section will help you understand the basics of HTML, so that you can create your own web pages or website.</p>
-                    <p><a href="#/html-tutorial/" target="_blank" class="btn btn-success">Learn More &raquo;</a></p>
-                </div>
-                <div class="col-md-4 col-xs-12">
-                    <h2>CSS</h2>
-                    <p>CSS is used for describing the presentation of web pages. The CSS tutorial section will help you learn the essentials of CSS, so that you can fine control the style and layout of your HTML document.</p>
-                    <p><a href="#/css-tutorial/" target="_blank" class="btn btn-success">Learn More &raquo;</a></p>
-                </div>
-                <div class="col-md-4 col-xs-12">
-                    <h2>Bootstrap</h2>
-                    <p>Bootstrap is a powerful front-end framework for faster and easier web development. The Bootstrap tutorial section will help you learn the techniques of Bootstrap so that you can create web your own website.</p>
-                    <p><a href="#/twitter-bootstrap-tutorial/" target="_blank" class="btn btn-success">Learn More &raquo;</a></p>
-                </div>
-            </div>
-
-            <div class="row p40">
-                <div id="map"></div>
             </div>
 
-            <hr/>
+            <div class="contact">
+                <div class="row p40">
+                    <div id="map"></div>
+                </div>
+            </div>
 
             <div class="row">
                 <div class="col-md-12">
